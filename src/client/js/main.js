fb38e3e244bd0d407b8fd4b8b65c580d3bbbee32
Batman@ZiiOS <aobphilos@gmail.com>
2017-10-02 22:11:24 +0700

    - add carousel lib 'flexslider2'

--- src/client/js/main.js
+++ src/client/js/main.js
@@ -283,6 +283,27 @@
 
     });
 
+    $('#carousel').flexslider({
+      animation: "slide",
+      controlNav: false,
+      animationLoop: false,
+      slideshow: false,
+      itemWidth: 210,
+      itemMargin: 5,
+      asNavFor: '#slider'
+    });
+
+    $('#slider').flexslider({
+      animation: "slide",
+      controlNav: false,
+      animationLoop: false,
+      slideshow: false,
+      sync: "#carousel",
+      start: function (slider) {
+        $('body').removeClass('loading');
+      }
+    });
+
   }); // end document ready ($)
 
 })(jQuery);
